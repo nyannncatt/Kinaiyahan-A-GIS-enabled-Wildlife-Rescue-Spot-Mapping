d3f26d29a5679d91fa1118e20008a2cb955b4b5f
nyannncatt <kamirelax187@gmail.com>
2025-09-13 17:49:03 +0800

    remove redundant import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import { useState, useEffect } from "react";
 import { supabase } from "./services/supabase";
 import Enforcement from "./pages/Enforcement";
