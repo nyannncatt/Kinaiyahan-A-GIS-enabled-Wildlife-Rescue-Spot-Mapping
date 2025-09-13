4d4f5b4d4e4c5691d05c5ef09ac8a09447221162
nyannncatt <kamirelax187@gmail.com>
2025-09-13 12:32:45 +0800

    test preview worked now changing to main version

--- src/sign-in-side/components/SignInCard.jsx
+++ src/sign-in-side/components/SignInCard.jsx
@@ -117,7 +117,7 @@ export default function SignInCard() {
     <>
       <Card variant="outlined">
         <Typography component="h1" variant="h4" sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}>
-          Signn in
+          Sign in
         </Typography>
 
         {loginError && (
