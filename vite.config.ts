4c193d658ccebe1162342df7d4b604e2791227ac
nyannncatt <kamirelax187@gmail.com>
2025-10-03 19:13:02 +0800

    search bar, species status filter marker and added new data setsand images both on marked and edit pop up

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      overlay: false,
+      timeout: 30000,
+    },
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+  },
 });
