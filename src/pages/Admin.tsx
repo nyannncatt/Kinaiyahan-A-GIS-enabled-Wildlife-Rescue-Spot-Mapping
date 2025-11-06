1b01866b6da3bca1018e26857329a61502ed5494
nyannncatt <kamirelax187@gmail.com>
2025-11-06 21:08:04 +0800

    added pets animation

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -55,8 +55,38 @@ function AdminComponent(props: { disableCustomTheme?: boolean }) {
             backgroundRepeat: 'no-repeat',
             backgroundSize: '100% 100%',
             backgroundAttachment: 'fixed',
+            position: 'relative',
           })}
         >
+          {environmentalBg && (
+            <Box className="bg-animals" sx={{ position: 'absolute', inset: 0, pointerEvents: 'none' }}>
+              {/* Right -> Left */}
+              <span className="animal rtl" title="Philippine Eagle" style={{ top: '6%', animationDuration: '22s', animationDelay: '0s', animationName: 'popFloatA' }}>🦅</span>
+              <span className="animal rtl" title="Philippine Crocodile" style={{ top: '12%', animationDuration: '27s', animationDelay: '0s', animationName: 'zigZagA' }}>🐊</span>
+              <span className="animal rtl" title="Whale Shark" style={{ top: '18%', animationDuration: '24s', animationDelay: '0s', animationName: 'popFloatA' }}>🦈</span>
+              <span className="animal rtl" title="Philippine Eagle-Owl" style={{ top: '24%', animationDuration: '29s', animationDelay: '0s', animationName: 'zigZagA' }}>🦉</span>
+              <span className="animal rtl" title="Philippine Deer" style={{ top: '30%', animationDuration: '26s', animationDelay: '0s', animationName: 'popFloatA' }}>🦌</span>
+              <span className="animal rtl" title="Carabao" style={{ top: '36%', animationDuration: '28s', animationDelay: '0s', animationName: 'zigZagA' }}>🐃</span>
+              <span className="animal rtl" title="Monkey" style={{ top: '42%', animationDuration: '25s', animationDelay: '0s', animationName: 'popFloatA' }}>🐒</span>
+              <span className="animal rtl" title="Butterfly" style={{ top: '48%', animationDuration: '26s', animationDelay: '0s', animationName: 'zigZagA' }}>🦋</span>
+              <span className="animal rtl" title="Frog" style={{ top: '54%', animationDuration: '23s', animationDelay: '0s', animationName: 'popFloatA' }}>🐸</span>
+              <span className="animal rtl" title="Otter" style={{ top: '60%', animationDuration: '27s', animationDelay: '0s', animationName: 'zigZagA' }}>🦦</span>
+              <span className="animal rtl" title="Parrot" style={{ top: '66%', animationDuration: '24s', animationDelay: '0s', animationName: 'popFloatA' }}>🦜</span>
+
+              {/* Left -> Right */}
+              <span className="animal ltr" title="Hawksbill Turtle" style={{ top: '10%', animationDuration: '24s', animationDelay: '0s', animationName: 'popFloatB' }}>🐢</span>
+              <span className="animal ltr" title="Tamaraw" style={{ top: '16%', animationDuration: '26s', animationDelay: '0s', animationName: 'zigZagB' }}>🐃</span>
+              <span className="animal ltr" title="Visayan Warty Pig" style={{ top: '22%', animationDuration: '23s', animationDelay: '0s', animationName: 'popFloatB' }}>🐗</span>
+              <span className="animal ltr" title="Philippine Tarsier" style={{ top: '28%', animationDuration: '29s', animationDelay: '0s', animationName: 'zigZagB' }}>🐵</span>
+              <span className="animal ltr" title="Philippine Hornbill" style={{ top: '34%', animationDuration: '27s', animationDelay: '0s', animationName: 'popFloatB' }}>🦅</span>
+              <span className="animal ltr" title="Dove" style={{ top: '40%', animationDuration: '22s', animationDelay: '0s', animationName: 'zigZagB' }}>🕊️</span>
+              <span className="animal ltr" title="Duck" style={{ top: '46%', animationDuration: '24s', animationDelay: '0s', animationName: 'popFloatB' }}>🦆</span>
+              <span className="animal ltr" title="Heron" style={{ top: '52%', animationDuration: '26s', animationDelay: '0s', animationName: 'zigZagB' }}>🪿</span>
+              <span className="animal ltr" title="Crab" style={{ top: '58%', animationDuration: '28s', animationDelay: '0s', animationName: 'popFloatB' }}>🦀</span>
+              <span className="animal ltr" title="Shrimp" style={{ top: '64%', animationDuration: '23s', animationDelay: '0s', animationName: 'zigZagB' }}>🦐</span>
+              <span className="animal ltr" title="Butterfly" style={{ top: '70%', animationDuration: '25s', animationDelay: '0s', animationName: 'popFloatB' }}>🦋</span>
+            </Box>
+          )}
           <Stack
             spacing={2}
             sx={{
@@ -86,7 +116,22 @@ function AdminComponent(props: { disableCustomTheme?: boolean }) {
               >
                 Admin Dashboard
               </Button>
-              <UserManagement />
+              <Box sx={{ position: 'relative' }}>
+                {environmentalBg && (
+                  <Box className="bg-animals" sx={{ position: 'absolute', inset: 0, pointerEvents: 'none' }}>
+                    {/* User Management focused floaters */}
+                    <span className="animal rtl" title="Bird" style={{ top: '8%', animationDuration: '18s', animationDelay: '0s', animationName: 'popFloatA' }}>🐦</span>
+                    <span className="animal ltr" title="Butterfly" style={{ top: '16%', animationDuration: '20s', animationDelay: '0s', animationName: 'zigZagB' }}>🦋</span>
+                    <span className="animal rtl" title="Duck" style={{ top: '24%', animationDuration: '19s', animationDelay: '0s', animationName: 'popFloatA' }}>🦆</span>
+                    <span className="animal ltr" title="Parrot" style={{ top: '32%', animationDuration: '21s', animationDelay: '0s', animationName: 'zigZagB' }}>🦜</span>
+                    <span className="animal rtl" title="Turtle" style={{ top: '40%', animationDuration: '22s', animationDelay: '0s', animationName: 'popFloatA' }}>🐢</span>
+                    <span className="animal ltr" title="Otter" style={{ top: '48%', animationDuration: '23s', animationDelay: '0s', animationName: 'zigZagB' }}>🦦</span>
+                    <span className="animal rtl" title="Dove" style={{ top: '56%', animationDuration: '18s', animationDelay: '0s', animationName: 'popFloatA' }}>🕊️</span>
+                    <span className="animal ltr" title="Crab" style={{ top: '64%', animationDuration: '20s', animationDelay: '0s', animationName: 'zigZagB' }}>🦀</span>
+                  </Box>
+                )}
+                <UserManagement />
+              </Box>
               {/* My Profile section */}
               <Box sx={{ height: 320 }} />
               <Box data-profile>
