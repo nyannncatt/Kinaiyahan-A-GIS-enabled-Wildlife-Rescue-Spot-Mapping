b20722f876f4799f6345aa88d29a718595eeacf0
nyannncatt <kamirelax187@gmail.com>
2025-11-13 17:12:41 +0800

    removal of redundant files

--- src/components-enfo/AdminHeader.tsx
+++ src/components-enfo/AdminHeader.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
-import NavbarBreadcrumbs from './NavbarBreadcrumbs';
+
 import ColorModeIconDropdown from '../shared-theme/ColorModeIconDropdown';
 
 export default function AdminHeader() {
@@ -9,15 +9,13 @@ export default function AdminHeader() {
       direction="row"
       sx={{
         display: { xs: 'none', md: 'flex' },
-        width: '100%',
         alignItems: { xs: 'flex-start', md: 'center' },
-        justifyContent: 'space-between',
-        maxWidth: { sm: '100%', md: '1700px' },
+        justifyContent: 'flex-end',
         pt: 1.5,
       }}
       spacing={2}
     >
-      <NavbarBreadcrumbs />
+   
       <Stack direction="row" sx={{ gap: 1 }}>
         <ColorModeIconDropdown />
       </Stack>
