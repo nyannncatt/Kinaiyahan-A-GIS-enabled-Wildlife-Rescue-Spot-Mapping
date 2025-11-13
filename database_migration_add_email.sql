4bc1b1c81bfb9b0a300a0c9fc29fea4458af4ebb
nyannncatt <kamirelax187@gmail.com>
2025-11-13 17:38:13 +0800

    imports changes

--- database_migration_add_email.sql
+++
@@ -1,7 +0,0 @@
--- Add email column to users table
-ALTER TABLE users 
-ADD COLUMN IF NOT EXISTS email TEXT;
-
--- Add a comment to the column
-COMMENT ON COLUMN users.email IS 'User email address';
-
